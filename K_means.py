ef157a1cdb54bf676e3ee47521a77df6a8c64738
agbossi <abossi@itba.edu.ar>
2021-11-20 22:27:30 -0300

    k means parece estar

--- K_means.py
+++ K_means.py
@@ -1,14 +1,20 @@
 import random
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import pyplot
+from mpl_toolkits.mplot3d import Axes3D
+
 
 def get_furthest_point(element_set, centroids):
-    distances = {}
-    for element in element_set:
-        for centroid in centroids:
-        d = np.linalg.norm(element - centroid)
-        distances[_class.get_class_value()] = d
-    closest_class = min(distances, key=distances.get)
-    return closest_class
+    point_distances = {}
+    for i in range(len(element_set)):
+        point_distances[i] = 0  # inicializo acumulador de distancias al cuadrado en 0
+        for j in range(len(centroids)):
+            d = np.linalg.norm(element_set[i] - centroids[j]) ** 2
+            point_distances[i] += d  # sumo la distancia de ese centroide al acumulador
+    # me quedo con el indice del elemento con mayor suma distancia al cuadrado a todos
+    furthest_index = max(point_distances, key=point_distances.get)
+    return element_set[furthest_index]
 
 
 class KMeansElement:
@@ -45,6 +51,9 @@ class KMeansClass:
     def set_centroid(self, centroid):
         self.centroid = centroid
 
+    def get_elements(self):
+        return self.elements
+
     def get_most_representative_label(self):
         if self.most_representative_label is None:
             self.calculate_most_representative_label()
@@ -73,7 +82,10 @@ class KMeansClass:
         for i in range(self.get_elements_amount()):
             for j in range(i+1, self.get_elements_amount()):
                 w = (self.elements[i].get_element() - self.elements[j].get_element()) ** 2
-        w /= self.get_elements_amount()
+        if self.get_elements_amount() != 0:
+            w /= self.get_elements_amount()
+        else:
+            w = 0
         return w
 
     def get_class_value(self):
@@ -83,6 +95,8 @@ class KMeansClass:
         return np.linalg.norm(self.get_centroid() - element.get_element())
 
     def calculate_centroid(self):
+        if len(self.elements) == 0:
+            return self.centroid
         new_centroid = 0
         for element in self.elements:
             new_centroid += element.get_element()
@@ -139,12 +153,24 @@ class KMeans:
         first_centroid = element_set[0]
         centroids.append(first_centroid)
         self.classes[0].set_centroid(first_centroid)
-        for _class in self.classes:
+        for i in range(1, len(self.classes)):
             next_centroid = get_furthest_point(element_set, centroids)
-            _class.set_centroid(next_centroid)
-        self.random_assignment(element_set, labels)
+            self.classes[i].set_centroid(next_centroid)
+            centroids.append(next_centroid)
+        self.point_assignment(element_set, labels)
+
+    def initialize_centroids(self, centroid_assignment, element_set, labels):
+        if centroid_assignment == 'random':
+            self.random_assignment(element_set, labels)
+        elif centroid_assignment == 'distance':
+            self.max_distance_assignment(element_set, labels)
 
     def random_assignment(self, element_set, labels):
+        for _class in self.classes:
+            _class.set_centroid(element_set[random.randint(0, len(element_set)-1)])
+        self.point_assignment(element_set, labels)
+
+    def point_assignment(self, element_set, labels):
         index = 0
         for element in element_set:
             class_value = random.randint(0, self.classes_amount-1)
@@ -166,7 +192,6 @@ class KMeans:
 
     def update_state(self):
         has_changes = False
-        self.calculate_centroids()
         for k_means_element in self.elements:
             closest_class = self.get_closest_class(k_means_element)
             if closest_class != k_means_element.get_class():
@@ -174,19 +199,44 @@ class KMeans:
                 self.classes[k_means_element.get_class()].remove_element(k_means_element)
                 self.classes[closest_class].add_element(k_means_element)
                 k_means_element.change_class(closest_class)
+        self.calculate_centroids()
         self.stationary = not has_changes
 
-    def fit(self, element_set, labels=None):
+    def fit(self, element_set, labels=None, centroid_assignment='random'):
         self.classes = [KMeansClass(None, i) for i in range(self.classes_amount)]
         if labels is None:
             labels = np.zeros(len(element_set))
         self.labels = labels
-        self.random_assignment(element_set, labels)
+        self.initialize_centroids(centroid_assignment, element_set, labels)
 
         while not self.stationary:
             self.update_state()
 
-    def print_result(self, y):
-        print("id       class        label")
-        for i in range(len(self.elements)):
-            print(self.elements[i].get_id(), "     ", self.elements[i].get_class(), "        ", y[i])
+    def predict_element(self, element):
+        distances = {}
+        for i in range(len(self.classes)):
+            distances[i] = self.classes[i].get_distance_to_class(KMeansElement(-1, element, -1, -1))
+        class_index = min(distances, key=distances.get)
+        return self.classes[class_index].get_most_representative_label()
+
+    def predict(self, elements):
+        predictions = []
+        for element in elements:
+            predictions.append(self.predict_element(element))
+        return np.array(predictions)
+
+    def plot_result(self, vertical_rot, horizontal_rot, labels):
+        fig = pyplot.figure()
+        ax = Axes3D(fig)
+        colors = ['blue', 'red', 'black']
+        color = 0
+        for _class in self.classes:
+            for element in _class.get_elements():
+                elem = element.get_element()
+                ax.scatter(elem[0], elem[1], elem[2], c=colors[color])
+            color += 1
+        plt.xlabel(labels[0])
+        plt.ylabel(labels[1])
+        ax.set_zlabel(labels[2])
+        ax.view_init(horizontal_rot, vertical_rot)
+        plt.show()
