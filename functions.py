48fe84b17457e4824dc8b78a3f5d033d05a4d3ac
agbossi <abossi@itba.edu.ar>
2021-11-21 16:05:06 -0300

    kohonen no rompe

--- functions.py
+++ functions.py
@@ -5,29 +5,45 @@ MEAN = 0
 STD = 1
 
 
+# η = 0,1 ∗(1 −epoca/max ctd epocas
 class LearningRateFunction:
-    def __init__(self):
-        self.learning_rate = 1
+    def __init__(self, max_learning_rate, iterations):
+        self.max_learning_rate = max_learning_rate
+        self.max_iterations = iterations
         self.count = 0
 
     def get_next_learning_rate(self):
-        self.count += 1
-        if self.learning_rate / (self.count / 5) <= 1:
-            self.learning_rate = self.learning_rate / (self.count / 5)
+        if self.count == 0:
+            return self.max_learning_rate
         else:
-            self.learning_rate = 1
-        return self.learning_rate
+            return self.max_learning_rate * (1 - self.count / self.max_iterations)
+    #def get_next_learning_rate(self):
+    #    self.count += 1
+    #    if self.learning_rate / (self.count / 5) <= 1:
+    #        self.learning_rate = self.learning_rate / (self.count / 5)
+    #    else:
+    #        self.learning_rate = 1
+    #    return self.learning_rate
 
 
+# R = (max ctd epocas −epoca) ∗Rinicial /max ctd epocas )
 class RadiusFunction:
-    def __init__(self, network_size, sorting_iterations_number):
-        self.init_radius = network_size / 2
+    def __init__(self, max_radius, iterations):
+        self.max_radius = max_radius
+        self.max_iterations = iterations
         self.count = 0
-        self.sorting_iterations_number = sorting_iterations_number
 
     def get_next_radius(self):
-        self.count += 1
-        return self.init_radius * (self.sorting_iterations_number - self.count) / self.sorting_iterations_number
+        return (self.max_iterations - self.count) * self.max_radius / self.max_iterations
+
+    #def __init__(self, network_size, sorting_iterations_number):
+    #    self.init_radius = network_size / 2
+    #    self.count = 0
+    #    self.sorting_iterations_number = sorting_iterations_number
+
+    #def get_next_radius(self):
+    #    self.count += 1
+    #    return self.init_radius * (self.sorting_iterations_number - self.count) / self.sorting_iterations_number
 
 
 def calculate_stats(df, column):
@@ -36,8 +52,8 @@ def calculate_stats(df, column):
     return [mean, std]
 
 
-def Standarizer(data_set):
-    cols = data_set.columns()
+def standarize(data_set):
+    cols = data_set.columns
     stats = [calculate_stats(data_set, column) for column in cols]
     data = data_set.to_numpy()
     for i in range(len(data)):
--- kohonen_network.py
+++ kohonen_network.py
@@ -6,7 +6,20 @@ import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors
-from sklearn.utils import shuffle
+from random import shuffle
+
+
+
+class KohonenElement:
+    def __init__(self, class_value, weights):
+        self.class_value = class_value
+        self.weights = weights
+
+    def get_class_value(self):
+        return self.class_value
+
+    def get_weights(self):
+        return self.weights
 
 
 class KohonenNetwork:
@@ -16,20 +29,24 @@ class KohonenNetwork:
         self.network_size = network_size
         self.type = network_type
         self.training_set = None
-        self.labels = {}
+
+    def create_kohonen_elements(self, data, labels):
+        training_set = []
+        for i in range(len(data)):
+            training_set.append(KohonenElement(labels[i], data[i]))
+        self.training_set = training_set
 
     # si hay datos no numericos que identifican, se asume que es la primera columna
-    def initialize(self, training_set, labels_map):
-        self.labels = labels_map
-        self.training_set = training_set
+    def initialize(self, training_set, labels):
+        self.create_kohonen_elements(training_set, labels)
         for i in range(self.network_size):
             for j in range(self.network_size):
-                self.network[i][j].weights = np.array(self.training_set[random.randint(0, len(self.training_set)-1)])
+                self.network[i][j].weights = self.training_set[random.randint(0, len(self.training_set)-1)].get_weights()
 
     def train(self, iterations, learning_rate_function, radius_function):
         for i in range(iterations):
             self.clear_marks()
-            self.training_set = shuffle(self.training_set)
+            shuffle(self.training_set)
             for training_example in self.training_set:
                 winner_neuron = self.get_representative(training_example)
                 vicinity = self.get_vicinity(winner_neuron, radius_function.get_next_radius())  # por ahora radio es cte
@@ -40,23 +57,35 @@ class KohonenNetwork:
         winner = None
         for i in range(self.network_size):
             for j in range(self.network_size):
+                curr_distance = self.network[i][j].get_euclidean_distance(input_example.get_weights())
+                if curr_distance < min_distance:
+                    min_distance = curr_distance
+                    winner = self.network[i][j]
+        winner.times_chosen += 1
+        winner.add_representative(input_example.get_class_value())
+        return winner
+
+    def get_representative_for_predict(self, input_example):
+        min_distance = sys.maxsize
+        winner = None
+        for i in range(self.network_size):
+            for j in range(self.network_size):
                 curr_distance = self.network[i][j].get_euclidean_distance(input_example)
                 if curr_distance < min_distance:
                     min_distance = curr_distance
                     winner = self.network[i][j]
         winner.times_chosen += 1
-        winner.represents.append(input_example)
         return winner
 
     def clear_marks(self):
         for i in range(self.network_size):
             for j in range(self.network_size):
                 self.network[i][j].times_chosen = 0
-                self.network[i][j].represents.clear()
+                self.network[i][j].clear_representatives()
 
     def update(self, vicinity, training_example, learning_rate):
         for neuron in vicinity:
-            delta = learning_rate * np.subtract(training_example, neuron.weights) * (1 / neuron.distance_from_winner)
+            delta = learning_rate * np.subtract(training_example.get_weights(), neuron.weights) * (1 / neuron.distance_from_winner)
             neuron.weights = np.add(neuron.weights, delta)
             neuron.distance_from_winner = sys.maxsize
 
@@ -66,10 +95,17 @@ class KohonenNetwork:
         for i in range(self.network_size):
             for j in range(self.network_size):
                 if i != winner_neuron.y_coord or j != winner_neuron.x_coord:
-                    self.network[i][j].distance_from_winner = winner_neuron.get_manhattan_grid_distance(self.network[i][j])
+                    self.network[i][j].distance_from_winner = winner_neuron.get_euclidean_grid_distance(self.network[i][j])
                     if self.network[i][j].distance_from_winner < radius:
                         vicinity.append(self.network[i][j])
         return vicinity
+
+    def predict(self, x_test, y_test):
+        results = []
+        for i in range(len(x_test)):
+            winner = self.get_representative_for_predict(x_test[i])
+            results.append([winner.get_most_represented_class(), y_test[i]])
+        return results
 
     def build_U_matrix(self):
         feature_map = [[self.get_average_neighbour_distance(self.network[i][j]) for j in range(self.network_size)] for i in range(self.network_size)]
@@ -80,14 +116,6 @@ class KohonenNetwork:
         plt.axis('off')
         plt.show()
 
-    def get_label(self, label_info):
-        for key in self.labels:
-            if np.array_equal(self.labels[key], label_info):
-                return key
-        return None
-# definitivamente hay un problema con el represents, agrega otra cosa, o hay una modificacion de dataset
-# o no se esta limpiando el represents
-
     def get_average_neighbour_distance(self, neuron):
         x_coord = neuron.x_coord
         y_coord = neuron.y_coord
@@ -95,7 +123,7 @@ class KohonenNetwork:
         distances_sum = 0
 
         # upper distance
-        if y_coord-1 > 0:
+        if y_coord-1 >= 0:
             distances_sum += self.network[y_coord-1][x_coord].get_euclidean_distance(neuron.weights)
             distances += 1
         # lower distance
@@ -103,7 +131,7 @@ class KohonenNetwork:
             distances_sum += self.network[y_coord+1][x_coord].get_euclidean_distance(neuron.weights)
             distances += 1
         # get left distance
-        if x_coord-1 > 0:
+        if x_coord-1 >= 0:
             distances_sum += self.network[y_coord][x_coord-1].get_euclidean_distance(neuron.weights)
             distances += 1
         # get right distance
@@ -123,7 +151,7 @@ class KohonenNetwork:
         neuron.avg_distance = distances_sum / distances
         return neuron.avg_distance
 
-    def plot_network_features(self, variable_names, standardizator=None):
+    def plot_network_features(self, variable_names):
         for k in range(self.input_size):
             feature_map = [[self.network[i][j].weights[k] for j in range(self.network_size)] for i in range(self.network_size)]
             plt.title(variable_names[k])
@@ -134,24 +162,26 @@ class KohonenNetwork:
             plt.show()
 
     def plot_clusters(self):
+        colors = ['blue', 'red', 'black']
         fig = plt.figure()
         ax = fig.add_subplot(111)
-        A = []#[x for x in range(self.network_size)]
-        B = []#[y for y in range(self.network_size)]  # esto tiene que tener la coordenada de los representantes
 
         for i in range(self.network_size):
             for j in range(self.network_size):
-                k = 0
-                for label in self.network[i][j].represents:
-                    A.append(j)
-                    B.append(i)
-                    ax.annotate('%s' % self.get_label(label), xy=(j, i), xytext=(10, 20-10*k), textcoords='offset points')  # xy es la label?
-                    k += 1
+                class_value = self.network[i][j].get_most_represented_class()
+                if class_value is None:
+                    c = 'black'
+                else:
+                    c = colors[class_value]
+                    ax.annotate('%s' % str(class_value), xy=(j, i), xytext=(10, 20 - 10),
+                                textcoords='offset points')  # xy es la label?
+                ax.scatter(j, i, c=c, s=70)
 
         plt.xlim(0, self.network_size-1)
         plt.ylim(0, self.network_size-1)
+        plt.xticks([i for i in range(self.network_size)])
+        plt.yticks([j for j in range(self.network_size)])
         plt.gca().invert_yaxis()
-        plt.scatter(A, B)
         plt.grid()
         plt.show()
 
@@ -173,7 +203,22 @@ class KohonenNeuron:
         self.avg_distance = sys.maxsize
         self.times_chosen = 0
         self.distance_from_winner = sys.maxsize
-        self.represents = []
+        self.represents = {}
+
+    def clear_representatives(self):
+        self.represents.clear()
+
+    def add_representative(self, class_value):
+        if class_value in self.represents:
+            self.represents[class_value] += 1
+        else:
+            self.represents[class_value] = 1
+
+    def get_most_represented_class(self):
+        if not self.represents:
+            return None
+        most_represented = max(self.represents, key=self.represents.get)
+        return most_represented
 
     def get_euclidean_distance(self, coord):
         return np.linalg.norm(self.weights - coord)
